bd6802aecd4edd2ab4239ffdd9e7adf4f6df5bdd
Juan Pablo Arce <juanjprot@gmail.com>
2020-08-20 17:54:32 -0300

    Fixed incorrect comparison in Matrix4x4f::operator!=

--- Src/Math/Matrix.cpp
+++ Src/Math/Matrix.cpp
@@ -64,7 +64,7 @@ bool Matrix4x4f::operator==(const Matrix4x4f& other) const {
 bool Matrix4x4f::operator!=(const Matrix4x4f& other) const {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            if (elements[i][j] == other.elements[i][j]) {
+            if (elements[i][j] != other.elements[i][j]) {
                 return true;
             }
         }
