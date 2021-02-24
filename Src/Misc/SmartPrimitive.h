5bf0e667678ba28ce808460c0dd671346f13eaee
Saalvage <29021710+Saalvage@users.noreply.github.com>
2021-02-24 05:24:15 +0100

    Expanded SmartPrimitives

--- Src/Misc/SmartPrimitive.h
+++ Src/Misc/SmartPrimitive.h
@@ -9,36 +9,26 @@
 
 // TODO: Dedicated SmartPointer/SmartArray?
 
+class OpContainer {
+    public:
+        virtual ~OpContainer() {};
+
+        virtual void exec() = 0;
+};
+
 class SmartBase {
     public:
         virtual ~SmartBase(){};
 };
 
-template <typename T, typename S=T>
-class SmartPrimitive : public SmartBase {
+template <typename T>
+class SmartBaseTwo : public SmartBase {
     public:
-        SmartPrimitive(const T& val) {
-            destructor = nullptr;
-            value = val;
-        }
-        SmartPrimitive(const T& val, void(*dtor)(const S&)) {
-            value = val;
-            destructor = dtor;
-        }
-        ~SmartPrimitive() {
-            if (destructor != nullptr) {
-                destructor((const S&)value);
-            }
-        }
-
         void set(const T& t) {
             value = t;
         }
 
         T& get() {
-            return value;
-        }
-        T& get() const {
             return value;
         }
 
@@ -48,38 +38,105 @@ class SmartPrimitive : public SmartBase {
 
     protected:
         T value;
+};
+
+template <typename T>
+class OpContainerFed : public OpContainer {
+    public:
+        void set(SmartBaseTwo<T>* val) {
+            value = val;
+        }
+
+    protected:
+        SmartBaseTwo<T>* value;
+};
+
+
+template <typename T, typename S=T>
+class SmartPrimitive : public SmartBaseTwo<T> {
+    public:
+        SmartPrimitive(const T& val) {
+            destructor = nullptr;
+            this->value = val;
+        }
+        SmartPrimitive(const T& val, void(*dtor)(const S&)) {
+            this->value = val;
+            destructor = dtor;
+        }
+        ~SmartPrimitive() {
+            if (destructor != nullptr) {
+                destructor((const S&)this->value);
+            }
+        }
+
+    protected:
         void(*destructor)(const S& t);
+};
+
+template <typename T>
+class SmartCustom : public SmartBaseTwo<T> {
+    public:
+        SmartCustom(const T& val, OpContainer* op) {
+            this->value = val;
+            delOp = op;
+        }
+        ~SmartCustom() {
+            delOp->exec();
+            delete delOp;
+        }
+
+    protected:
+        OpContainer* delOp;
+};
+
+template <typename T, typename... Args>
+class SmartCustomExtra : public SmartBaseTwo<T>{
+    public:
+        SmartCustomExtra(const T& val, Args... args, void(*dtor)(const T&, Args...)) {
+            this->value = val;
+            // TODO: Somehow get rid of duplicate data?
+            arguments = std::make_tuple(val, args...);
+            destructor = dtor;
+        }
+        ~SmartCustomExtra() {
+            std::get<0>(arguments) = this->value;
+            std::apply(destructor, arguments);
+        }
+
+    private:
+        std::tuple<T, Args...> arguments;
+        void(*destructor)(const T&, Args...);
 };
 
 template <typename T>
 class SmartRef {
     public:
-    // Get.
-    T& operator()() {
-        return sp->get();
-    }
-    const T& operator()() const {
-        return sp->get();
-    }
-    // Set.
-    void operator=(const T& t) {
-        sp->set(t);
-    }
-    // Get as pointer.
-    T* operator&() {
-        return sp->getPointer();
-    }
-    T operator->() {
-        return sp->get();
-    }
+        // Get.
+        T& operator()() {
+            return sb->get();
+        }
+        const T& operator()() const {
+            return sb->get();
+        }
+        // Set.
+        void operator=(const T& t) {
+            sb->set(t);
+        }
+        // Get as pointer.
+        T* operator&() {
+            return sb->getPointer();
+        }
+        T operator->() {
+            return sb->get();
+        }
 
-    SmartRef() {}
-    SmartRef(SmartPrimitive<T>* sp) {
-        this->sp = sp;
-    }
+        SmartRef() {}
+        SmartRef(SmartBaseTwo<T>* sb) {
+            this->sb = sb;
+        }
 
     private:
-        SmartPrimitive<T>* sp;
+        SmartBaseTwo<T>* sb;
 };
 
 template <typename T>
@@ -94,19 +151,12 @@ static void clearPointerArray(std::vector<T*> const& v) {
     }
 }
 
-class OpContainer {
-    public:
-        virtual ~OpContainer() {};
-
-        virtual void exec() = 0;
-};
-
 class SmartOrderedDestructor {
     public:
         SmartOrderedDestructor(int size) {
             preop = nullptr;
             postop = nullptr;
-            objects = new SmartBase*[size];;
+            objects = new SmartBase*[size];
             index = 0;
         }
 
@@ -156,6 +206,25 @@ class SmartOrderedDestructor {
         SmartRef<T> getReferencePointer(Args&&... args) {
             objects[index] = new SmartPrimitive(new T(args...), deletePointer);
             return SmartRef<T>((SmartPrimitive<T>*)objects[index++]);
+        }
+
+        // TODO: Remove? Unused.
+        template <typename T>
+        SmartRef<T> getReferenceCustom(const T& t, OpContainer* op) {
+            objects[index] = new SmartCustom(t, op);
+            OpContainerFed<T>* feedOp = dynamic_cast<OpContainerFed<T>*>(op);
+            if (feedOp != nullptr) {
+                feedOp->set((SmartCustom<T>*)objects[index]);
+            }
+            return SmartRef<T>((SmartCustom<T>*)objects[index++]);
+        }
+
+        // TODO: &&?
+        // TODO: Work with pointers for args?
+        template <typename T, typename... Args>
+        SmartRef<T> getReference(const T& t, Args... args, void(*dtor)(const T&, Args...)) {
+            objects[index] = new SmartCustomExtra<T, Args...>(t, args..., dtor);
+            return SmartRef<T>((SmartCustomExtra<T, Args...>*)objects[index++]);
         }
     
         template <typename T>
