de3bce2a253dfed066006e2db9aa6297ab1a9736
Saalvage <29021710+Saalvage@users.noreply.github.com>
2021-04-07 00:45:25 +0200

    Add proper resource management base classes

--- Src/ResourceManagement/VK.h
+++ Src/ResourceManagement/VK.h
@@ -9,36 +9,56 @@
 
 namespace PGE {
 
-class VKBuffer : public Resource<vk::Buffer> {
-	private:
+template <class T>
+class VKResource : public Resource<T> {
+	public:
 		vk::Device device;
 
 	public:
-		VKBuffer(vk::Device dev, vk::DeviceSize size, vk::BufferUsageFlags bufferUsage) {
+		VKResource(vk::Device dev) {
 			device = dev;
-
-			vk::BufferCreateInfo bufferInfo = vk::BufferCreateInfo({}, size, bufferUsage, vk::SharingMode::eExclusive);
-			resource = device.createBuffer(bufferInfo);
 		}
 
-		~VKBuffer() {
-			device.destroy(resource);
+		VKResource(Graphics* gfx) {
+			device = ((GraphicsVK*)gfx)->getDevice();
+		}
+};
+
+template <class T>
+class VKDestroyResource : public VKResource<T> {
+	public:
+		using VKResource<T>::VKResource;
+
+		~VKDestroyResource() {
+			this->device.destroy(this->resource);
+		}
+};
+
+template <class T>
+class VKFreeResource : public VKResource<T> {
+	public:
+		using VKResource<T>::VKResource;
+
+		~VKFreeResource() {
+			this->device.free(this->resource);
+		}
+};
+
+class VKBuffer : public VKDestroyResource<vk::Buffer> {
+	public:
+		VKBuffer(vk::Device dev, vk::DeviceSize size, vk::BufferUsageFlags bufferUsage) : VKDestroyResource(dev) {
+			vk::BufferCreateInfo bufferInfo = vk::BufferCreateInfo({}, size, bufferUsage, vk::SharingMode::eExclusive);
+			resource = device.createBuffer(bufferInfo);
 		}
 
 		__RES_MNGMT__REF_FACT_METH(VKBuffer)
 };
 
-class VKMemory : public Resource<vk::DeviceMemory> {
-	private:
-		vk::Device device;
-
+class VKMemory : public VKFreeResource<vk::DeviceMemory> {
 	public:
-		VKMemory(Graphics* gfx, vk::Buffer buffer, vk::MemoryPropertyFlags memProps) {
-			GraphicsVK* graphics = (GraphicsVK*)gfx;
-			device = graphics->getDevice();
-
+		VKMemory(Graphics* gfx, vk::Buffer buffer, vk::MemoryPropertyFlags memProps) : VKFreeResource(gfx) {
 			vk::MemoryRequirements memReq = device.getBufferMemoryRequirements(buffer);
-			int memType = graphics->findMemoryType(memReq.memoryTypeBits, memProps);
+			int memType = ((GraphicsVK*)gfx)->findMemoryType(memReq.memoryTypeBits, memProps);
 
 			vk::MemoryAllocateInfo memoryInfo = vk::MemoryAllocateInfo(memReq.size, memType);
 			resource = device.allocateMemory(memoryInfo);
@@ -46,25 +66,16 @@ class VKMemory : public Resource<vk::DeviceMemory> {
 			device.bindBufferMemory(buffer, resource, 0);
 		}
 
-		~VKMemory() {
-			device.free(resource);
-		}
-
 		__RES_MNGMT__REF_FACT_METH(VKMemory)
 };
 
-class VKPipeline : public Resource<vk::Pipeline> {
+class VKPipeline : public VKDestroyResource<vk::Pipeline> {
 	private:
 		static constexpr vk::PipelineInputAssemblyStateCreateInfo inputAssemblyLines = vk::PipelineInputAssemblyStateCreateInfo({}, vk::PrimitiveTopology::eLineList, false);
 		static constexpr vk::PipelineInputAssemblyStateCreateInfo inputAssemblyTris = vk::PipelineInputAssemblyStateCreateInfo({}, vk::PrimitiveTopology::eTriangleList, false);
 
-		vk::Device device;
-
 	public:
-		VKPipeline(Graphics* gfx, ShaderVK* shader, Primitive::TYPE primitive) {
-			GraphicsVK* graphics = (GraphicsVK*)gfx;
-			device = graphics->getDevice();
-				
+		VKPipeline(Graphics* gfx, ShaderVK* shader, Primitive::TYPE primitive) : VKDestroyResource(gfx) {
 			const vk::PipelineInputAssemblyStateCreateInfo* inputInfo;
 			switch (primitive) {
 				case Primitive::TYPE::LINE: {
@@ -75,49 +86,27 @@ class VKPipeline : public Resource<vk::Pipeline> {
 					inputInfo = &inputAssemblyTris;
 				} break;
 			}
-			resource = graphics->createPipeline(shader, inputInfo);
-		}
-
-		~VKPipeline() {
-			device.destroy(resource);
+			resource = ((GraphicsVK*)gfx)->createPipeline(shader, inputInfo);
 		}
 
 		__RES_MNGMT__REF_FACT_METH(VKPipeline)
 };
 
-class VKPipelineLayout : public Resource<vk::PipelineLayout> {
-	private:
-			vk::Device device;
-
+class VKPipelineLayout : public VKDestroyResource<vk::PipelineLayout> {
 	public:
-		VKPipelineLayout(vk::Device dev, const std::vector<vk::PushConstantRange>& ranges) {
-			device = dev;
-
+		VKPipelineLayout(vk::Device dev, const std::vector<vk::PushConstantRange>& ranges) : VKDestroyResource(dev) {
 			vk::PipelineLayoutCreateInfo layoutInfo({}, 0, nullptr, (uint32_t)ranges.size(), ranges.data());
 			resource = device.createPipelineLayout(layoutInfo);
-		}
-
-		~VKPipelineLayout() {
-			device.destroy(resource);
 		}
 
 		__RES_MNGMT__REF_FACT_METH(VKPipelineLayout)
 };
 
-class VKShader : public Resource<vk::ShaderModule> {
-	private:
-		vk::Device device;
-
+class VKShader : public VKDestroyResource<vk::ShaderModule> {
 	public:
-		VKShader(vk::Device dev, const std::vector<uint8_t>& shaderBinary) {
-			device = dev;
-
+		VKShader(vk::Device dev, const std::vector<uint8_t>& shaderBinary) : VKDestroyResource(dev) {
 			vk::ShaderModuleCreateInfo shaderCreateInfo = vk::ShaderModuleCreateInfo({}, shaderBinary.size(), (uint32_t*)shaderBinary.data());
 			resource = device.createShaderModule(shaderCreateInfo);
-		}
-
-		~VKShader() {
-			device.destroy(resource);
 		}
 
 		__RES_MNGMT__REF_FACT_METH(VKShader)
