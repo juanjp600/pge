0b3b190f4fc4ee45f05f3e0e72cebd06f8dcc19f
Saalvage <29021710+Saalvage@users.noreply.github.com>
2021-04-10 21:44:09 +0200

    New error management system

--- Include/Exception/Exception.h
+++ Include/Exception/Exception.h
@@ -7,16 +7,47 @@ namespace PGE {
 
 class Exception {
     public:
-        Exception();
-        Exception(const Exception& ex);
-        Exception(String src, String detail);
-        String getSource() const;
-        String getDetails() const;
+        // TODO: Remove.
+        Exception(const String&, const String&) { }
+        Exception() { }
+        //
+
+        Exception(const char* file, int line, const String& extra = "");
+
+        const String& what() const;
 
     private:
-        String source;
-        String details;
+        String info;
 };
+
+#define __EX_THRW(E) if (!E) throw PGE::Exception(__FILE__, __LINE__)
+#define __EX_THRW_I(E, I) if (!E) throw PGE::Exception(__FILE__, __LINE__, I) 
+
+#ifdef EXCEPTION_CATCH_HIGH
+    #define EXCEPTION_CATCH_MID
+    #define ASSERT_H(E) __EX_THRW(E)
+    #define ASSERT_H_I(E, I) __EX_THRW_I(E, I)
+#else
+    #define ASSERT_H(E)
+    #define ASSERT_H_I(E, I)
+#endif
+
+#ifdef EXCEPTION_CATCH_MID
+    #define EXCEPTION_CATCH_LOW
+    #define ASSERT_M(E) __EX_THRW(E)
+    #define ASSERT_M_I(E, I) __EX_THRW_I(E, I)
+#else
+    #define ASSERT_M(E)
+    #define ASSERT_M_I(E, I)
+#endif
+
+#ifdef EXCEPTION_CATCH_LOW
+    #define ASSERT_L(E) __EX_THRW(E)
+    #define ASSERT_L_I(E, I) __EX_THRW_I(E, I)
+#else
+    #define ASSERT_L(E)
+    #define ASSERT_L_I(E, I)
+#endif
 
 }
 
