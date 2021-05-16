d380c233ebc105bcebdc11c73ddd08234b144bec
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-05-16 05:01:22 +0200

    Add Math.h with pi and e

---
+++ Include/Math/Math.h
@@ -0,0 +1,9 @@
+#ifndef PGE_MATH_H_INCLUDED
+#define PGE_MATH_H_INCLUDED
+
+namespace Math {
+	static constexpr float PI = 3.1415926535f;
+	static constexpr float E =  2.7182818284f;
+}
+
+#endif // PGE_MATH_H_INCLUDED
