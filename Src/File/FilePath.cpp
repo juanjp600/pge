fc36f9dfb5cf8d4dab4d1f24c8d021fa2220f8b2
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-06-21 20:32:10 +0200

    Solve resourceStr static initialization fiasco

--- Src/File/FilePath.cpp
+++ Src/File/FilePath.cpp
@@ -28,10 +28,9 @@ static String sanitizeFileSeperator(const String& str) {
     return str.replace("\\", "/");
 }
 
-static String resourceStr;
-
 // Already sanitized.
-static String getResourceStr() {
+static String& getResourceStr() {
+    static String resourceStr;
     if (resourceStr.byteLength() == 0) {
 #if defined(__APPLE__) && defined(__OBJC__)
         NSBundle* bundle = [NSBundle mainBundle];
