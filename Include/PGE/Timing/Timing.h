91f600f80de610c079cbe86d3ff9cbed62906e14
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-08-14 04:09:42 +0200

    Consistent differentiating between struct and class

--- Include/PGE/Timing/Timing.h
+++ Include/PGE/Timing/Timing.h
@@ -16,7 +16,7 @@ class Timing {
 	private:
 		using Clock = std::chrono::steady_clock;
 
-		class Time** current;
+		struct Time** current;
 		const String name;
 		const std::chrono::time_point<Clock> start;
 };
