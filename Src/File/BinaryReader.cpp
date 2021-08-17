b9cfee64d94b0f83379851da0a7906852cc74d8c
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-08-17 05:20:44 +0200

    Fix read/write integer template instantiation

--- Src/File/BinaryReader.cpp
+++ Src/File/BinaryReader.cpp
@@ -18,14 +18,15 @@ bool BinaryReader::tryRead(T& out) {
 }
 
 #define PGE_IO_DEFAULT_SPEC(T) template bool BinaryReader::tryRead(T& out)
-PGE_IO_DEFAULT_SPEC(u8);
-PGE_IO_DEFAULT_SPEC(u16);
-PGE_IO_DEFAULT_SPEC(u32);
-PGE_IO_DEFAULT_SPEC(u64);
-PGE_IO_DEFAULT_SPEC(i8);
-PGE_IO_DEFAULT_SPEC(i16);
-PGE_IO_DEFAULT_SPEC(i32);
-PGE_IO_DEFAULT_SPEC(i64);
+PGE_IO_DEFAULT_SPEC(byte);
+PGE_IO_DEFAULT_SPEC(short);
+PGE_IO_DEFAULT_SPEC(int);
+PGE_IO_DEFAULT_SPEC(long);
+PGE_IO_DEFAULT_SPEC(long long);
+PGE_IO_DEFAULT_SPEC(unsigned short);
+PGE_IO_DEFAULT_SPEC(unsigned int);
+PGE_IO_DEFAULT_SPEC(unsigned long);
+PGE_IO_DEFAULT_SPEC(unsigned long long);
 PGE_IO_DEFAULT_SPEC(float);
 PGE_IO_DEFAULT_SPEC(double);
 PGE_IO_DEFAULT_SPEC(long double);
