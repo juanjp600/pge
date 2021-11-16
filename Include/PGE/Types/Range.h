5e79e5fd65d3705b3cb18179018731de6b1d3183
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-11-16 12:47:43 +0100

    Better range iterator dereferencing

--- Include/PGE/Types/Range.h
+++ Include/PGE/Types/Range.h
@@ -6,7 +6,7 @@ namespace PGE {
 template <typename SIZE = int>
 class Range {
 	public:
-		class RangeIterator {
+		class Iterator {
 			public:
 				using difference_type = SIZE;
 				using value_type = SIZE;
@@ -14,12 +14,15 @@ class Range {
 				using reference = SIZE&;
 				using iterator_category = std::random_access_iterator_tag;
 
-				static constexpr RangeIterator begin(const Range& range) { return { range.start, range.step }; }
-				static constexpr RangeIterator end(const Range& range) { return { range.stop, range.step }; }
+				static constexpr Iterator begin(const Range& range) { return { range.start, range.step }; }
+				static constexpr Iterator end(const Range& range) { return { range.stop, range.step }; }
 
-				constexpr SIZE operator*() const { return position; }
+				operator int&() { return position; }
+
+				constexpr const Iterator& operator*() const { return *this; }
+				constexpr Iterator& operator*() { return *this; }
 				constexpr SIZE operator[](SIZE offset) const { return position + step * offset; }
-				constexpr SIZE operator-(const RangeIterator& other) const { return position - other.position; }
+				constexpr SIZE operator-(const Iterator& other) const { return position - other.position; }
 
 				constexpr void operator++() { position += step; }
 				constexpr void operator--() { position -= step; }
@@ -28,19 +31,19 @@ class Range {
 				constexpr void operator+=(SIZE steps) { position += step * steps; }
 				constexpr void operator-=(SIZE steps) { position -= step * steps; }
 
-				constexpr const RangeIterator operator+(SIZE steps) const { return RangeIterator(position + step * steps, step); }
-				constexpr const RangeIterator operator-(SIZE steps) const { return RangeIterator(position - step * steps, step); }
+				constexpr const Iterator operator+(SIZE steps) const { return Iterator(position + step * steps, step); }
+				constexpr const Iterator operator-(SIZE steps) const { return Iterator(position - step * steps, step); }
 
 				// TODO: C++20 <=>
-				constexpr bool operator<(const RangeIterator& other) const { return position < other.position; }
-				constexpr bool operator>(const RangeIterator& other) const { return position > other.position; }
-				constexpr bool operator<=(const RangeIterator& other) const { return position <= other.position; }
-				constexpr bool operator>=(const RangeIterator& other) const { return position >= other.position; }
-				constexpr bool operator==(const RangeIterator& other) const { return position == other.position; }
-				constexpr bool operator!=(const RangeIterator& other) const { return position != other.position; }
+				constexpr bool operator<(const Iterator& other) const { return position < other.position; }
+				constexpr bool operator>(const Iterator& other) const { return position > other.position; }
+				constexpr bool operator<=(const Iterator& other) const { return position <= other.position; }
+				constexpr bool operator>=(const Iterator& other) const { return position >= other.position; }
+				constexpr bool operator==(const Iterator& other) const { return position == other.position; }
+				constexpr bool operator!=(const Iterator& other) const { return position != other.position; }
 
 			private:
-				constexpr RangeIterator(SIZE position, SIZE step) : position(position), step(step) { }
+				constexpr Iterator(SIZE position, SIZE step) : position(position), step(step) { }
 
 				SIZE position;
 				SIZE step;
@@ -50,8 +53,8 @@ class Range {
 		constexpr Range(SIZE end) : stop(end) { }
 		constexpr Range(SIZE begin, SIZE end, SIZE step = 1) : start(begin), stop(end), step(step) { }
 
-		constexpr RangeIterator begin() { return RangeIterator::begin(*this); }
-		constexpr RangeIterator end() { return RangeIterator::end(*this); }
+		constexpr Iterator begin() { return Iterator::begin(*this); }
+		constexpr Iterator end() { return Iterator::end(*this); }
 
 	private:	
 		SIZE start = 0;
