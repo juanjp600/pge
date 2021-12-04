45eaba69d8d3625377d5fe0a7cca9f320350ac02
Salvage <29021710+Saalvage@users.noreply.github.com>
2021-12-05 00:37:21 +0100

    Better and more tests!

--- Tests/StringTests.cpp
+++ Tests/StringTests.cpp
@@ -1,30 +1,66 @@
-#include <doctest/doctest.h>
+#include "Util.h"
 
 #include <PGE/String/String.h>
+#include <PGE/Exception/Exception.h>
 
 using namespace PGE;
 
 TEST_SUITE("Strings") {
 
 TEST_CASE("Very basic tests") {
+	CHECK(String().byteLength() == 0);
+	CHECK(String().length() == 0);
+
+	CHECK(String() == "");
+	CHECK(String().equalsIgnoreCase(""));
+
 	String empty;
-	CHECK_EQ(empty.byteLength(), 0);
-	CHECK_EQ(empty.length(), 0);
+	CHECK(empty.begin() == empty.end());
+	CHECK(empty.rbegin() == empty.rend());
+
+	CHECK(empty.begin() != empty.rbegin());
+	CHECK(empty.end() != empty.rend());
+	CHECK(empty.rbegin() != empty.end());
+	CHECK(empty.begin() != empty.rend());
+}
+
+TEST_CASE("Basic iterator tests") {
+	String a;
+	SUBCASE("A") {
+		a = "A";
+	}
+	SUBCASE("Ä") {
+		a = u8"Ä";
+	}
+
+	CHECK(a.begin() == a.rbegin());
+	CHECK(a.begin() + 1 == a.end());
+	CHECK(a.begin() == a.end() - 1);
+	CHECK(a.rbegin() + 1 == a.rend());
+	CHECK(a.rbegin() == a.rend() - 1);
 }
 
 TEST_CASE("Multiplication") {
-	String a = "A";
-	CHECK_EQ(a.multiply(0), String());
-	CHECK_EQ(a.multiply(1), a);
-	CHECK_EQ(a.multiply(5), "AAAAA");
+	String a; String sep;
+	SUBCASE_PR(
+		SUBCASE_P(sep, "", ", "),
+		a, "A", u8"Ä"
+	);
+
+	CHECK(a.multiply(0) == String());
+	CHECK(a.multiply(1) == a);
+	
 }
 
-TEST_CASE("Multiplication with seperator") {
+TEST_CASE("Multiplication explicit") {
 	String a = "A";
-	String sep = ", ";
-	CHECK_EQ(a.multiply(0, sep), String());
-	CHECK_EQ(a.multiply(1, sep), a);
-	CHECK_EQ(a.multiply(5, sep), "A, A, A, A, A");
+
+	CHECK(a.multiply(5) == "AAAAA");
+	CHECK(a.multiply(5, ", ") == "A, A, A, A, A");
+
+	String u = u8"Ä";
+	CHECK(u.multiply(5) == u8"ÄÄÄÄÄ");
+	CHECK(u.multiply(5, ", ") == u8"Ä, Ä, Ä, Ä, Ä");
 }
 
 }
